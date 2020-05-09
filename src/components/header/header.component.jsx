92f4e761be694905ef1581467b4ba31c13f1c6b0
Akansha Tiwari <akanshat1999@gmail.com>
2020-05-09 19:46:27 +0530

    cart icon added and cart-dropdown components to application, also implemented cart-reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
+import CartIcon from '../cart-icon/cart-icon.component'
 //connect is a higher order component that lets us
 //modify our component to have access to redux high
 //order comp. are fucntions that take
@@ -8,11 +10,13 @@ import { Link } from 'react-router-dom'
 
 import { auth } from '../../firebase/firebase.utils'
 
+import CartDropDown from '../cart-dropdown/cart-dropdown.component'
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -33,15 +37,18 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropDown />}
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 })
 
 //now the curretnUSer we see passed is coming
 //from this mapsate to props, due to "connect"
 //now we can remobe currentuser we passed in app.js
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux'
 
 import userReducer from './user/user.reducer'
-
+import cartReducer from './cart/cart.reducer'
 export default combineReducers({
-    user: userReducer
-    //will return one giant object
-    //now we bring our new root-reducer to our
-    //store and then store to provider in index.js
-
+  user: userReducer,
+  cart: cartReducer
+  //will return one giant object
+  //now we bring our new root-reducer to our
+  //store and then store to provider in index.js
 })
