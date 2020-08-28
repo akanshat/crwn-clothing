1516c6dadeaa7a5f0ba82157ac42b61fbc478ee0
akanshat <akanshat1999@gmail.com>
2020-08-29 01:22:43 +0530

    add item and remove item to checkout page

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -10,3 +10,13 @@ export const addItem = (item) => ({
   //payload can be anything, item we are trying
   //to add to our cart array is passed here
 });
+
+export const removeItem = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+})
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
