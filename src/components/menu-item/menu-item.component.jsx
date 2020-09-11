1cf36eb6d4d6d1628f912a24a8a1e9b2de4f8b1d
akanshat <akanshat1999@gmail.com>
2020-09-12 00:32:42 +0530

    update typo in menu-item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -16,7 +16,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='sub-title'>SHOPNOW</span>
+      <span className='sub-title'>SHOP NOW</span>
     </div>
   </div>
 )
